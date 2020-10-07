42471bf2f5a6475b50f555b13da34468954a3950
SimonFredrich <21simplex02@gmail.com>
2020-10-07 22:35:52 +0200

    basic image read and show + turning pixel black

--- opencv-02/main.cpp
+++ opencv-02/main.cpp
@@ -8,7 +8,34 @@
 using namespace cv;
 using namespace std;
 
+// definiere Bildpfad Konstante
+#define IMAGE "./assets/vektor_mit_koordinatensystem.png"
+
 int main()
 {
-	return 0;
+	Mat img = imread(IMAGE);
+	cout << "Dimensionen: " << img.cols << "x" << img.rows << endl;
+
+	// Fehler beim lesen der Bilddatei
+	if (img.empty())
+	{
+		cout << "Die Datei konnte nicht gelesen werden." << endl;
+		return 1;
+	}
+
+	// Überschreibe Pixel mit Schwarz
+	for (size_t col = 0; col < img.cols; col+=2)
+	{
+		for (size_t row = 0; row < img.rows; row+=2)
+		{
+			img.at<Vec3b>(row, col) = (0, 0, 0);
+		}
+	}
+
+	// Zeige das Ergebnis
+	imshow("img", img);
+	if (waitKey(0))
+	{
+		return 0;
+	}
 }
