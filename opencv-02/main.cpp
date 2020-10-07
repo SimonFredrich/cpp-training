fc25f95741d1608d2400984d814e22ed6b09c785
SimonFredrich <21simplex02@gmail.com>
2020-10-07 22:09:53 +0200

    init project

---
+++ opencv-02/main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	return 0;
+}
